df7aa9d94764916449d348d2c9392f991e1df53e
Ivan Shabanov <Ivan.shabanov@gmail.com>
2017-04-24 10:54:22 +0700

    Add files via upload

---
+++ scroll_to_up.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+  /* Настройки скрипта */
+  /* Высота прокрутки после которой показывается стрелка наверх */
+  var height_to_show_scrollup = 200;
+  /* Установим стили для кнопки */
+  var styles = {
+    "position": "fixed",
+    /* Положение стрелки */
+    /* "left": "50px", */
+    /* "top": "50px", */
+    "right": "50px",
+    "bottom": "50px",
+    /* Ширина и высота стрелки */
+    "width": "50px",
+    "height": "50px",
+    /* Установим картинку стрелки наверх */
+    "background": "url('/images/scroll_to_up.png')",
+    "background-repeat": "no-repeat",
+    "background-size": "cover",
+    
+    "opacity": "0",
+    "cursor": "pointer"
+    
+  };
+
+  /* Добавим элемент кнопки наверх */
+  $('body').append('<div class="scrollup"></div>');
+
+  /* Применим стили к кнопке */
+  $('.scrollup').css( styles );  
+  
+  /* Поставим на кнопку "наверх" действие  */
+  $('.scrollup').click( function(){
+    $("html, body").animate({ scrollTop: 0 }, 500); 
+    return false;
+  });
+
+  /* Если у нас страница уже немного прокруена вниз показать кнопку "наверх" */
+  if ( $(document).scrollTop() > height_to_show_scrollup ) {
+    $('.scrollup').animate({'opacity': 0.7}, 300);;
+  };
+
+  /* При прокрутке кнопка наверх либо появляется либо изчезает */
+  $(window).scroll(function(){
+    $('.scrollup').stop();
+    if ( $(document).scrollTop() > height_to_show_scrollup ) {
+      $('.scrollup').animate({'opacity': 0.7}, 300);;
+    } else {
+      $('.scrollup').animate({'opacity': 0}, 300);;
+    }
+  });
+  $('.scrollup').hover(function(){
+    if ( $(document).scrollTop() > height_to_show_scrollup ) { 
+      $('.scrollup').stop();
+      $('.scrollup').animate({'opacity': 1}, 100);;
+    }; 
+  }, function(){
+    $('.scrollup').stop();
+    if ( $(document).scrollTop() > height_to_show_scrollup ) {
+      $('.scrollup').animate({'opacity': 0.7}, 100);;
+    } else {
+      $('.scrollup').animate({'opacity': 0}, 300);;
+    }
+  });
+
+
+});
+
+
